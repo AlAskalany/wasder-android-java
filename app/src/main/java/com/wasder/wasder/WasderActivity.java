fdc40c958764ee551e78287dd2e9d737bc0ad1a1
Ahmed AlAskalany <ahmed.alaskalany@gmail.com>
2017-10-14 00:56:56 +0200

    Butterknife bindings for WasderActivity

--- app/src/main/java/com/wasder/wasder/WasderActivity.java
+++ app/src/main/java/com/wasder/wasder/WasderActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
@@ -25,14 +24,34 @@ import com.wasder.wasder.viewmodel.WasderActivityViewModel;
 
 import java.util.Collections;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class WasderActivity extends AppCompatActivity implements LifecycleOwner, NavigationView
         .OnNavigationItemSelectedListener, FirebaseAuth.AuthStateListener {
 
+    @SuppressWarnings("unused")
     private static final String TAG = "WasderActivity";
     private static final int RC_SIGN_IN = 9001;
 
+    @BindView(R.id.message2)
+    TextView mTextMessage;
+
+    @BindView(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
+
+    @BindView(R.id.nav_view)
+    NavigationView mNavigationView;
+
+    @BindView(R.id.navigation2)
+    BottomNavigationView mBottomNavigationView;
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+
     private WasderActivityViewModel mViewModel;
-    private TextView mTextMessage;
+
     private BottomNavigationView.OnNavigationItemSelectedListener
             mOnNavigationItemSelectedListener = new BottomNavigationView
             .OnNavigationItemSelectedListener() {
@@ -58,33 +77,21 @@ public class WasderActivity extends AppCompatActivity implements LifecycleOwner,
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wasder);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
+        setSupportActionBar(mToolbar);
 
         // View model
         mViewModel = ViewModelProviders.of(this).get(WasderActivityViewModel.class);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string
-                .navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R
+                .string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        mNavigationView.setNavigationItemSelectedListener(this);
 
-        mTextMessage = findViewById(R.id.message2);
-        BottomNavigationView navigation = findViewById(R.id.navigation2);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        mBottomNavigationView.setOnNavigationItemSelectedListener
+                (mOnNavigationItemSelectedListener);
     }
 
     @Override
@@ -93,6 +100,7 @@ public class WasderActivity extends AppCompatActivity implements LifecycleOwner,
         // Start sign in if necessary
         if (shouldStartSignIn()) {
             startSignIn();
+            //noinspection UnnecessaryReturnStatement
             return;
         }
     }
@@ -113,11 +121,16 @@ public class WasderActivity extends AppCompatActivity implements LifecycleOwner,
         mViewModel.setIsSigningIn(true);
     }
 
+    @OnClick(R.id.fab)
+    public void submit(View view) {
+        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction
+                ("Action", null).show();
+    }
+
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
         }
@@ -147,7 +160,7 @@ public class WasderActivity extends AppCompatActivity implements LifecycleOwner,
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
@@ -165,8 +178,7 @@ public class WasderActivity extends AppCompatActivity implements LifecycleOwner,
 
         }
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
+        mDrawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
 
