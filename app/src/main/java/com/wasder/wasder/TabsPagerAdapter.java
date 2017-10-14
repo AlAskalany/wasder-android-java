e252c134a7edc953f4c6f6b4f539a18d0209dce2
Ahmed AlAskalany <ahmed.alaskalany@gmail.com>
2017-10-14 02:04:02 +0200

    Created Navigation and Tab fragments

---
+++ app/src/main/java/com/wasder/wasder/TabsPagerAdapter.java
@@ -0,0 +1,42 @@
+package com.wasder.wasder;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Ahmed AlAskalany on 10/14/2017.
+ * Wasder AB
+ */
+public class TabsPagerAdapter extends FragmentPagerAdapter {
+
+    List<Fragment> mFragmentList = new ArrayList<>();
+    List<String> mTitleList = new ArrayList<>();
+
+    public TabsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mTitleList.get(position);
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        mFragmentList.add(fragment);
+        mTitleList.add(title);
+    }
+}
--- app/src/main/java/com/wasder/wasder/WasderActivity.java
+++ app/src/main/java/com/wasder/wasder/WasderActivity.java
@@ -3,6 +3,7 @@ package com.wasder.wasder;
 import android.arch.lifecycle.LifecycleOwner;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -10,10 +11,8 @@ import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.util.SparseArrayCompat;
 import android.support.v4.view.GravityCompat;
-import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -26,57 +25,45 @@ import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 import com.wasder.wasder.viewmodel.WasderActivityViewModel;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class WasderActivity extends AppCompatActivity implements LifecycleOwner, NavigationView
-        .OnNavigationItemSelectedListener, FirebaseAuth.AuthStateListener {
+public class WasderActivity extends AppCompatActivity implements LifecycleOwner, NavigationView.OnNavigationItemSelectedListener, FirebaseAuth.AuthStateListener, NavigationFragment.OnFragmentInteractionListener, TabFragment.OnFragmentInteractionListener {
 
     @SuppressWarnings("unused")
     private static final String TAG = "WasderActivity";
     private static final int RC_SIGN_IN = 9001;
-
+    private final SparseArrayCompat<NavigationFragment> mNavFragments = new SparseArrayCompat<>();
     @BindView(R.id.drawer_layout)
     DrawerLayout mDrawerLayout;
-
     @BindView(R.id.nav_view)
     NavigationView mNavigationView;
-
     @BindView(R.id.navigation2)
     BottomNavigationView mBottomNavigationView;
-
     @BindView(R.id.toolbar)
     Toolbar mToolbar;
-
     @BindView(R.id.tabLayout)
     TabLayout mTabLayout;
-
-    @BindView(R.id.viewPager)
-    ViewPager mViewPager;
-
     private WasderActivityViewModel mViewModel;
-
     private BottomNavigationView.OnNavigationItemSelectedListener
             mOnNavigationItemSelectedListener = new BottomNavigationView
             .OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    //mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    //mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    //mTextMessage.setText(R.string.title_notifications);
-                    return true;
+            if (getCurrentNavigationFragment() != null) {
+                getSupportFragmentManager().saveFragmentInstanceState
+                        (getCurrentNavigationFragment());
+            }
+            NavigationFragment fragment = mNavFragments.get(item.getItemId());
+            if (fragment != getCurrentNavigationFragment()) {
+                //mActionBar.setTitle(fragment.getFragmentTitle());
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment)
+                        .addToBackStack(null).commit();
+                return true;
             }
             return false;
         }
@@ -102,12 +89,15 @@ public class WasderActivity extends AppCompatActivity implements LifecycleOwner,
         mBottomNavigationView.setOnNavigationItemSelectedListener
                 (mOnNavigationItemSelectedListener);
 
-        // Setup Tabs
-        TabsPagerAdapter pagerAdapter = new TabsPagerAdapter(getSupportFragmentManager());
-        pagerAdapter.addFragment(new Fragment(), "Home");
-        pagerAdapter.addFragment(new Fragment(), "Other");
-        mViewPager.setAdapter(pagerAdapter);
-        mTabLayout.setupWithViewPager(mViewPager);
+        // Setup BottomNavigationView with NavigationFragments
+        mNavFragments.put(R.id.navigation_home, new HomeNavigationFragment());
+        mNavFragments.put(R.id.navigation_dashboard, new LiveNavigationFragment());
+        getSupportFragmentManager().beginTransaction().add(R.id.container, mNavFragments.get(R.id
+                .navigation_home), "Home").commit();
+    }
+
+    private Fragment getCurrentNavigationFragment() {
+        return getSupportFragmentManager().findFragmentById(R.id.container);
     }
 
     @Override
@@ -214,33 +204,8 @@ public class WasderActivity extends AppCompatActivity implements LifecycleOwner,
         }
     }
 
-    public class TabsPagerAdapter extends FragmentPagerAdapter {
+    @Override
+    public void onFragmentInteraction(Uri uri) {
 
-        List<Fragment> mFragmentList = new ArrayList<>();
-        List<String> mTitleList = new ArrayList<>();
-
-        public TabsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return mFragmentList.get(position);
-        }
-
-        @Override
-        public int getCount() {
-            return mFragmentList.size();
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return mTitleList.get(position);
-        }
-
-        public void addFragment(Fragment fragment, String title) {
-            mFragmentList.add(fragment);
-            mTitleList.add(title);
-        }
     }
 }
