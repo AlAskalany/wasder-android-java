38c758c5c1dbe96607546206d04158c6e513617d
Ahmed AlAskalany <ahmed.alaskalany@gmail.com>
2017-10-15 11:28:18 +0200

    Navigation drawer menu items

--- app/src/main/java/com/wasder/wasder/WasderActivity.java
+++ app/src/main/java/com/wasder/wasder/WasderActivity.java
@@ -226,17 +226,17 @@ public class WasderActivity extends AppCompatActivity implements LifecycleOwner,
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_profile) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_friends) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_followers) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_achievements) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_settings_account) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_settings_notifications) {
 
         }
 
