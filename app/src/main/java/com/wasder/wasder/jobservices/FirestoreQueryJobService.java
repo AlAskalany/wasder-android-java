fa93942410a3104ea56e777cea8749ec157a140d
Ahmed AlAskalany <ahmed.alaskalany@gmail.com>
2017-10-18 18:06:03 +0200

    test app

--- app/src/main/java/com/wasder/wasder/jobservices/FirestoreQueryJobService.java
+++ app/src/main/java/com/wasder/wasder/jobservices/FirestoreQueryJobService.java
@@ -1,19 +1,9 @@
 package com.wasder.wasder.jobservices;
 
-import android.content.Context;
-import android.util.Log;
-
 import com.firebase.jobdispatcher.JobParameters;
 import com.firebase.jobdispatcher.JobService;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.QuerySnapshot;
-import com.wasder.wasder.R;
-
-import java.util.Map;
 
 
 /**
@@ -27,29 +17,13 @@ public class FirestoreQueryJobService extends JobService {
 
     @Override
     public boolean onStartJob(final JobParameters jobParameters) {
-        mThread = new Thread(new Runnable() {
+        Runnable mRunnable = mThread = new Thread(new Runnable() {
             @Override
             public void run() {
-                Context context = getApplicationContext();
-                String appName = context.getString(R.string.app_name);
                 try {
-                    //This task takes 2 seconds to complete.
-                    //Thread.sleep(2000);
-                    Log.d("FirestoreJobService", appName);
                     FirebaseFirestore firestore = FirebaseFirestore.getInstance();
-                    Query query = firestore.collection("restaurants");
-                    //query.whereEqualTo("category", "Mexican");
-                    query.limit(50);
-                    query.addSnapshotListener(new EventListener<QuerySnapshot>() {
-                        @Override
-                        public void onEvent(QuerySnapshot documentSnapshots,
-                                            FirebaseFirestoreException e) {
-                            for (DocumentSnapshot snapshot : documentSnapshots) {
-                                Map<String, Object> data = snapshot.getData();
-                                Log.d("OnStartJob", "onEvent: " + data);
-                            }
-                        }
-                    });
+                    String collectionName = null;
+                    Query query = firestore.collection(collectionName);
                 } finally {
                     //Tell the framework that the job has completed and doesnot needs to be
                     // reschedule
