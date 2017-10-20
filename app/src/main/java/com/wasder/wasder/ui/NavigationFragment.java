1816fa36531def2829b291030e2763842a395cb5
Ahmed AlAskalany <ahmed.alaskalany@gmail.com>
2017-10-20 16:36:35 +0200

    Theme

--- app/src/main/java/com/wasder/wasder/ui/NavigationFragment.java
+++ app/src/main/java/com/wasder/wasder/ui/NavigationFragment.java
@@ -15,10 +15,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -215,15 +211,15 @@ public class NavigationFragment extends Fragment implements NavigationView
         appBarLayout = view.findViewById(R.id.appbar);
         //appBarLayout.post(createRunnable(appBarLayout, mAnimationListener));
         Toolbar toolbar = view.findViewById(R.id.toolbar);
-        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
-        ActionBar actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
-        actionBar.setDisplayShowHomeEnabled(true);
+        //((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        //ActionBar actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
+        //actionBar.setDisplayShowHomeEnabled(true);
 
-        DrawerLayout drawer = getActivity().findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(getActivity(), drawer, toolbar,
-                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
+        //DrawerLayout drawer = getActivity().findViewById(R.id.drawer_layout);
+        //ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(getActivity(), drawer, toolbar,
+        //R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        //drawer.addDrawerListener(toggle);
+        //toggle.syncState();
 
         NavigationView navigationView = getActivity().findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
