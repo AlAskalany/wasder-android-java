3ed3430284f9bbd60c02720bb7a69ecda8758ec5
Ahmed AlAskalany <ahmed.alaskalany@gmail.com>
2017-10-31 18:54:45 +0100

    Profile Own

--- app/src/main/java/co/wasder/wasder/ProfileActivity.java
+++ app/src/main/java/co/wasder/wasder/ProfileActivity.java
@@ -1,13 +1,25 @@
 package co.wasder.wasder;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    CollapsingToolbarLayout collapsingToolbarLayout;
+    ImageView profilePhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +31,27 @@ public class ProfileActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser user = auth.getCurrentUser();
+        collapsingToolbarLayout = findViewById(R.id.collapsingToolbarLayout);
+        if (user != null) {
+            String displayName = user.getDisplayName();
+            if (!TextUtils.isEmpty(displayName)) {
+                collapsingToolbarLayout.setTitle(displayName);
+            }
+            profilePhoto = findViewById(R.id.profilePhoto);
+            Uri photoUri = user.getPhotoUrl();
+            if (photoUri != null) {
+                String photoUrl = photoUri.toString();
+                if (!TextUtils.isEmpty(photoUrl)) {
+                    Glide.with(this)
+                            .load(photoUri)
+                            .transition(DrawableTransitionOptions.withCrossFade())
+                            .into(profilePhoto);
+                }
+            }
+        }
+
     }
 
     @Override
