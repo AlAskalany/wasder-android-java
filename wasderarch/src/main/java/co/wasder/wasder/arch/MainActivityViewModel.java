8d7c382a0cf46c30dcff283b07845e300f26e535
Ahmed AlAskalany <ahmed.alaskalany@gmail.com>
2017-11-07 11:47:06 +0100

    Java 8, ViewModel for MainActivity

---
+++ wasderarch/src/main/java/co/wasder/wasder/arch/MainActivityViewModel.java
@@ -0,0 +1,32 @@
+package co.wasder.wasder.arch;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+/**
+ * Created by Ahmed AlAskalany on 11/7/2017.
+ * Navigator
+ */
+
+public class MainActivityViewModel extends ViewModel {
+
+    private MutableLiveData<FirebaseAuth> auth;
+    private MutableLiveData<FirebaseUser> user;
+
+    public LiveData<FirebaseUser> getUser() {
+        if (user == null) {
+            user = new MutableLiveData<>();
+            loadUser();
+        }
+        return user;
+    }
+
+    private void loadUser() {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        user.setValue(auth.getCurrentUser());
+    }
+}
