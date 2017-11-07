5a6a390d560d356dc5fb7d80b84c910f1b33b7d6
Ahmed AlAskalany <ahmed.alaskalany@gmail.com>
2017-11-07 16:12:54 +0100

    Anonymous sign in

--- wasderarch/src/main/java/co/wasder/wasder/arch/SplashActivity.java
+++ wasderarch/src/main/java/co/wasder/wasder/arch/SplashActivity.java
@@ -2,45 +2,68 @@ package co.wasder.wasder.arch;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.ErrorCodes;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.Arrays;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class SplashActivity extends AppCompatActivity {
 
     private static final int RC_SIGN_IN = 100;
-
-    @BindView(R.id.activities)
-    RecyclerView mActivities;
+    private static final String TAG = "SplashActivity";
+    @BindView(R.id.logoImageView)
+    ImageView mLogoImageView;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         ButterKnife.bind(this);
-
-        FirebaseAuth auth = FirebaseAuth.getInstance();
-        if (auth.getCurrentUser() != null) {
-            startActivity(new Intent(this, MainActivity.class));
-            finish();
-        } else {
-            startActivityForResult(AuthUI.getInstance()
-                    .createSignInIntentBuilder()
-                    .setIsSmartLockEnabled(false)
-                    .setAvailableProviders(Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI
-                            .EMAIL_PROVIDER)
-                            .build(), new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
-                    .build(), RC_SIGN_IN);
+        mAuth = FirebaseAuth.getInstance();
+        if (mAuth.getCurrentUser() != null) {
+            startActivity(MainActivity.createIntent(SplashActivity.this, null));
         }
+    }
+
+    @OnClick(R.id.signInButton)
+    public void signIn(View view) {
+        startActivityForResult(AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setIsSmartLockEnabled(false)
+                .setAvailableProviders(Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI
+                        .EMAIL_PROVIDER)
+                        .build(), new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                .build(), RC_SIGN_IN);
+    }
+
+    @OnClick(R.id.skipButton)
+    public void skip(View view) {
+        mAuth.signInAnonymously().addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    startActivity(MainActivity.createIntent(SplashActivity.this, null));
+                } else {
+                    Log.d(TAG, "onComplete: Anonymous Sign In Failed");
+                }
+            }
+        });
     }
 
     @Override
@@ -53,6 +76,8 @@ public class SplashActivity extends AppCompatActivity {
 
             // Successfully signed in
             if (resultCode == RESULT_OK) {
+                String providerType = response.getProviderType();
+                Log.d(TAG, "onActivityResult: Provider Type: " + providerType);
                 startActivity(MainActivity.createIntent(this, response));
                 finish();
                 return;
