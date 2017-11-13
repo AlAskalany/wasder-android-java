c7c4b91beae632b5efadf3879ffc145da67f2629
Ahmed AlAskalany <ahmed.alaskalany@gmail.com>
2017-11-13 19:25:35 +0100

    tab fragment binding

--- app/src/main/java/co/wasder/wasder/fragment/tab/FeedTabFragment.java
+++ app/src/main/java/co/wasder/wasder/fragment/tab/FeedTabFragment.java
@@ -5,19 +5,35 @@ import android.os.Bundle;
 import android.support.annotation.Keep;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.firebase.ui.common.ChangeEventType;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import co.wasder.data.base.BaseModel;
+import co.wasder.data.model.FeedModel;
 import co.wasder.ui.base.BaseTabFragmentViewModel;
 import co.wasder.ui.viewmodel.FeedTabFragmentViewModel;
 import co.wasder.wasder.R;
 import co.wasder.wasder.base.BaseTabFragment;
 import co.wasder.wasder.dialogfragment.AddFirestoreItemDialogFragment;
+import co.wasder.wasder.listener.OnFirestoreItemSelectedListener;
+import co.wasder.wasder.recycleradapter.FeedRecyclerAdapter;
 import co.wasder.wasder.util.Dialogs;
+import co.wasder.wasder.viewholder.FeedViewHolder;
 
 /**
  * Created by Ahmed AlAskalany on 10/30/2017.
@@ -36,6 +52,32 @@ public class FeedTabFragment extends BaseTabFragment {
     public String USERS;
     public AddFirestoreItemDialogFragment mAddPostDialog;
     public BaseTabFragmentViewModel mViewModel;
+    @BindView(R.id.recyclerView)
+    public RecyclerView mRecyclerView;
+    @NonNull
+    protected OnFirestoreItemSelectedListener onFirestoreItemSelectedListener = new
+            OnFirestoreItemSelectedListener() {
+        @Override
+        public void onFirestoreItemSelected(BaseModel event, View itemView) {
+
+        }
+
+        @Override
+        public void onChildChanged(ChangeEventType type, DocumentSnapshot snapshot, int newIndex,
+                                   int oldIndex) {
+
+        }
+
+        @Override
+        public void onDataChanged() {
+
+        }
+
+        @Override
+        public void onError(FirebaseFirestoreException e) {
+
+        }
+    };
     private long LIMIT;
 
     public static FeedTabFragment newInstance(int sectionNumber, String title) {
@@ -58,5 +100,50 @@ public class FeedTabFragment extends BaseTabFragment {
         mAddPostDialog = Dialogs.AddPostDialogFragment();
         setupSearchAndFilters();
         return view;
+    }
+
+    protected void attachRecyclerViewAdapter() {
+        final FirestoreRecyclerOptions<FeedModel> options = new FirestoreRecyclerOptions
+                .Builder<FeedModel>()
+                .setQuery(mQuery, FeedModel.class)
+                .build();
+        final RecyclerView.Adapter<FeedViewHolder> adapter = FeedRecyclerAdapter.getAdapter(this,
+                onFirestoreItemSelectedListener, mQuery, FeedModel.class);
+
+        // Scroll to bottom on new messages
+        adapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
+            @Override
+            public void onItemRangeInserted(final int positionStart, final int itemCount) {
+                assert mRecyclerView != null;
+                mRecyclerView.smoothScrollToPosition(0);
+            }
+        });
+        assert mRecyclerView != null;
+        mRecyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onAuthStateChanged(@NonNull final FirebaseAuth firebaseAuth) {
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            attachRecyclerViewAdapter();
+        } else {
+            Toast.makeText(getContext(), R.string.signing_in, Toast.LENGTH_SHORT).show();
+            firebaseAuth.signInAnonymously()
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        }
+                    });
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            attachRecyclerViewAdapter();
+        }
+        FirebaseAuth.getInstance().addAuthStateListener(this);
     }
 }
