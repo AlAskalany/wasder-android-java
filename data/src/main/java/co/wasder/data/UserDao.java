a702bc4c053b71a3fb27bee51631b0e521304c46
Ahmed AlAskalany <ahmed.alaskalany@gmail.com>
2017-11-26 14:26:25 +0100

    updated gradle plugin

---
+++ data/src/main/java/co/wasder/data/UserDao.java
@@ -0,0 +1,12 @@
+package co.wasder.data;
+
+import android.arch.persistence.room.Dao;
+
+/**
+ * Created by Ahmed AlAskalany on 11/13/2017.
+ * Navigator
+ */
+@Dao
+public interface UserDao {
+
+}
---
+++ data/src/main/java/co/wasder/data/WasderTypeConverters.java
@@ -0,0 +1,10 @@
+package co.wasder.data;
+
+/**
+ * Created by Ahmed AlAskalany on 11/13/2017.
+ * Navigator
+ */
+
+public class WasderTypeConverters {
+
+}
