d0ea86ba53abc8bdbf6016a93ede7ad3afcb0902
Ahmed AlAskalany <ahmed.alaskalany@gmail.com>
2018-01-13 20:50:03 +0100

    WasderApplication class

---
+++ app/src/main/java/co/wasder/wasder/di/WasderApplication.java
@@ -0,0 +1,12 @@
+package co.wasder.wasder.di;
+
+import android.app.Application;
+
+/**
+ * Created by Ahmed AlAskalany on 1/13/2018.
+ * Navigator
+ */
+
+public class WasderApplication extends Application {
+
+}
